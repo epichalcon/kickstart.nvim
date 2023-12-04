54d614ab0ffb997c4129f51d4f8832d78b45d2d5
epichalcon <nicolashirra115@gmail.com>
2023-12-04 20:47:55 +0100

    modificacion del init

--- init.lua
+++ init.lua
@@ -1,11 +1,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require("custom.lazy")
-require("custom.options")
-require("custom.keymaps")
-require("custom.config.LSP")
-require("custom.config.utils")
+require("core.plugins")
+require("core.options")
+require("core.keymaps")
+require("core.config")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
